8a81138f927b7a362a838bcaeb272aeff8b3580a
kingdomappsolutions <justin@kingdomappsolutions.tech>
2022-04-19 16:58:26 -0500

    Initial commit

---
+++ mail.php
@@ -0,0 +1,23 @@
+<?php 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $name = testInput($_POST["Name"]);
+      $phone = testInput($_POST["Phone"]);
+      $email = testInput($_POST["Email"]);
+      $option = testInput($_POST["Option"]);
+      $message = testInput($_POST["Message"]);
+    }
+
+    function testInput($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+
+	$formcontent="From: $name \n\n Phone Number: $phone \n\n Subject: $option \n\n Message: $message";
+	$recipient = "justin@kingdomappsolutions.tech";
+	$subject = "Contact Form Submission";
+	$mailheader = "From: $name <$email> \r\n";
+	mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+	header("Location:https://kingdomappsolutions.tech/success.html");
+?>
